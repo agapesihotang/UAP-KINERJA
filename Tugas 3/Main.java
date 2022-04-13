48b09ae2b9dd7c3d1c8d007578f17ce443aeeeb5
agapesihotang <103532445+agapesihotang@users.noreply.github.com>
2022-04-13 10:05:08 +0700

    Create Main.java

---
+++ Tugas 3/Main.java
@@ -0,0 +1,27 @@
+//nama: Stanley Agape Sihotang
+//kelas: TI-A
+//judul: Toko Baju
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Baju.tampilan();
+        //instansiasi
+        Baju tokobaju=new Baju();
+        //input
+        Scanner input=new Scanner(System.in);
+
+
+
+        System.out.print("Baju yang akan anda beli bertipe : ");
+        tokobaju.Jenis_Baju=input.nextLine();
+        System.out.print("Jumlah Baju yang akan anda beli adalah : ");
+        tokobaju.jumlah=input.nextInt();
+
+        System.out.println("Jenis  yang anda beli : "+tokobaju.Jenis_Baju);
+        tokobaju.harga();
+        tokobaju.menghitung(tokobaju.jumlah);
+
+
+    }
+}
