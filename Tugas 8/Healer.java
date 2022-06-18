a5bfc0a5a093e87ce8f3d04fd9414af3522dd2d5
agapesihotang <103532445+agapesihotang@users.noreply.github.com>
2022-06-19 05:37:27 +0700

    Create Healer.java

---
+++ Tugas 8/Healer.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception_handling;
+
+/**
+ *
+ * @author Personal
+ */
+   public class Healer extends Character{
+       
+       
+    public Healer() {
+             setHP(70);
+             setAttack(10);
+             setDefense(10);
+       }
+
+    public void heal() {
+            setHP(getHP() + 25);
+    }
+ 
+    @Override
+    public boolean attack() {
+        int Acak = (int) (Math.random() * 100) + 1;
+            if (Acak <= 85) {
+                 return true;
+            } else {
+                 return false;
+            }
+    }
+    @Override
+    public void info() {
+            super.info();
+            System.out.println("Role\t: Healer");
+            System.out.println("HP\t: " + getHP());
+            System.out.println("Attack\t: " + getAttack());
+            System.out.println("Defense\t: " + getDefense());
+    }
+}
